8c79522d36cb8cfe9ae60e5592cf94c3159d65e0
Bresiu <michal.brewczak@gmail.com>
2013-09-10 01:40:30 +0200

    marsz, bieg i powtorzenia

--- Minutnik/src/main/java/pl/bresiu/minutnik/AlarmReceiver.java
+++ Minutnik/src/main/java/pl/bresiu/minutnik/AlarmReceiver.java
@@ -4,21 +4,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
-    public static String ACTION_ALARM = "com.alarammanager.alaram";
+    public static String ACTION_ALARM = "pl.bresiu.minutnik";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Interwał", Toast.LENGTH_SHORT).show();
         Bundle bundle = intent.getExtras();
         String action = bundle.getString(ACTION_ALARM);
         if (action.equals(ACTION_ALARM)) {
             Intent inService = new Intent(context, TaskService.class);
             context.startService(inService);
-        } else {
-            Toast.makeText(context, "Else loop", Toast.LENGTH_SHORT).show();
         }
     }
 }
--- Minutnik/src/main/java/pl/bresiu/minutnik/MainActivity.java
+++ Minutnik/src/main/java/pl/bresiu/minutnik/MainActivity.java
@@ -3,7 +3,6 @@ package pl.bresiu.minutnik;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -12,14 +11,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
-    final String PREFERENCES_NAME = "Preferences";
-    final String IS_WORKING = "isWorking";
-    EditText interwal;
-    Button startstop;
+public class MainActivity extends Activity implements View.OnClickListener {
+    final String ALARMS = "alarms";
+    final String PREFERENCES_NAME = "preferences";
     SharedPreferences preferences;
     SharedPreferences.Editor preferencesEditor;
+    EditText marsz;
+    EditText bieg;
+    EditText powtorzenia;
+    Button startstop;
+    Button zeruj;
+    ArrayList<PendingIntent> intentArray = new ArrayList<PendingIntent>();
+    AlarmManager[] alarmManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +38,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     public void setWidgets() {
-        interwal = (EditText) findViewById(R.id.interwal);
+        marsz = (EditText) findViewById(R.id.minuty_marszu);
+        bieg = (EditText) findViewById(R.id.minuty_biegu);
+        powtorzenia = (EditText) findViewById(R.id.liczba_powtorzen);
+        zeruj = (Button) findViewById(R.id.zero);
         startstop = (Button) findViewById(R.id.startstop);
         startstop.setText((isWorking()) ? "STOP" : "START");
     }
@@ -40,68 +50,147 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.startstop:
-                if (!isWorking()) {
-                    setWork(true);
-                    Toast.makeText(getApplicationContext(), "Wystartowano", Toast.LENGTH_SHORT).show();
-                    startstop.setText("STOP");
-                    btnStartSchedule(v);
-                } else {
-                    setWork(false);
-                    Toast.makeText(getApplicationContext(), "Zastopowano", Toast.LENGTH_SHORT).show();
+                if (isWorking()) {
+                    preferencesEditor.putInt(ALARMS, 0);
                     startstop.setText("START");
-                    btnCancelSchedules(v);
+                    deleteSchedule();
+                } else {
+                    startstop.setText("STOP");
+                    makeSchedule();
+                }
+                break;
+            case R.id.zero:
+                marsz.setText("0");
+                bieg.setText("0");
+                powtorzenia.setText("0");
+                break;
+            case R.id.button:
+                if (isInt(marsz)) {
+                    setM(marsz);
+                }
+                break;
+            case R.id.button2:
+                if (isInt(marsz)) {
+                    setP(marsz);
+                }
+                break;
+            case R.id.button3:
+                if (isInt(bieg)) {
+                    setM(bieg);
+                }
+                break;
+            case R.id.button4:
+                if (isInt(bieg)) {
+                    setP(bieg);
+                }
+                break;
+            case R.id.button5:
+                if (isInt(powtorzenia)) {
+                    setM(powtorzenia);
+                }
+                break;
+            case R.id.button6:
+                if (isInt(powtorzenia)) {
+                    setP(powtorzenia);
                 }
                 break;
         }
     }
 
-    public int getInterwal() {
-        return Integer.parseInt(interwal.getText().toString()) * 60000;
-    }
-
-    public boolean isWorking() {
-        return preferences.getBoolean(IS_WORKING, false);
-    }
-
-    public void setWork(boolean work) {
-        preferencesEditor.putBoolean(IS_WORKING, work);
-        preferencesEditor.commit();
-    }
-
-    public void btnStartSchedule(View v) {
-
+    private boolean isInt(EditText editText) {
         try {
-            AlarmManager alarms = (AlarmManager) this
-                    .getSystemService(Context.ALARM_SERVICE);
-
-            Intent intent = new Intent(getApplicationContext(),
-                    AlarmReceiver.class);
-            intent.putExtra(AlarmReceiver.ACTION_ALARM,
-                    AlarmReceiver.ACTION_ALARM);
-
-            final PendingIntent pIntent = PendingIntent.getBroadcast(this,
-                    1234567, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-            alarms.setRepeating(AlarmManager.RTC_WAKEUP,
-                    System.currentTimeMillis(), getInterwal(), pIntent);
-        } catch (Exception e) {
+            Integer.parseInt(editText.getText().toString());
+            return true;
+        } catch (NumberFormatException e) {
             e.printStackTrace();
+            return false;
+        } catch (java.lang.NullPointerException l) {
+            l.printStackTrace();
+            return false;
         }
     }
 
-    public void btnCancelSchedules(View v) {
+    private void setM(EditText editText) {
+        int tmp = Integer.parseInt(editText.getText().toString());
+        if (tmp > 0) {
+            editText.setText("" + --tmp);
+        }
+    }
 
-        Intent intent = new Intent(getApplicationContext(),
-                AlarmReceiver.class);
-        intent.putExtra(AlarmReceiver.ACTION_ALARM,
-                AlarmReceiver.ACTION_ALARM);
+    private void setP(EditText editText) {
+        int tmp = Integer.parseInt(editText.getText().toString());
+        editText.setText("" + ++tmp);
+    }
 
-        final PendingIntent pIntent = PendingIntent.getBroadcast(this, 1234567,
-                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    public boolean isWorking() {
+        return preferences.getInt(ALARMS, 0) != 0;
+    }
 
-        AlarmManager alarms = (AlarmManager) this
-                .getSystemService(Context.ALARM_SERVICE);
+    public List<Integer> makeList() {
+        List<Integer> list = new ArrayList<Integer>();
 
-        alarms.cancel(pIntent);
+        int numberOfRepeats = 0;
+        int minutesOfMarch = 0;
+        int minutesOfRun = 0;
+        //TODO
+        if (powtorzenia.getText().toString().matches("") ||
+                marsz.getText().toString().matches("") ||
+                bieg.getText().toString().matches("")) {
+            return null;
+        } else {
+            try {
+                numberOfRepeats = Integer.parseInt(powtorzenia.getText().toString());
+                minutesOfMarch = Integer.parseInt(marsz.getText().toString());
+                minutesOfRun = Integer.parseInt(bieg.getText().toString());
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                return null;
+            } catch (java.lang.NullPointerException l) {
+                l.printStackTrace();
+                return null;
+            }
+            for (int i = 0; i < numberOfRepeats; i++) {
+                list.add(minutesOfMarch);
+                list.add(minutesOfRun);
+            }
+            return list;
+        }
+    }
+
+    public void makeSchedule() {
+        List<Integer> list = makeList();
+        if (list == null) {
+            Toast.makeText(this, "Wprowadź wszystkie dane!", Toast.LENGTH_SHORT).show();
+            preferencesEditor.putInt(ALARMS, 0);
+            startstop.setText("START");
+            deleteSchedule();
+        } else {
+            deleteSchedule();
+            alarmManager = new AlarmManager[list.size()];
+            Calendar cal = Calendar.getInstance();
+            for (int i = 0; i < list.size(); i++) {
+                cal.add(Calendar.SECOND, list.get(i));
+                Intent intent = new Intent(this, AlarmReceiver.class);
+                intent.putExtra(AlarmReceiver.ACTION_ALARM, AlarmReceiver.ACTION_ALARM);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(this, i, intent, 0);
+                alarmManager[i] = (AlarmManager) getSystemService(ALARM_SERVICE);
+                alarmManager[i].set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
+                intentArray.add(pendingIntent);
+            }
+            int times = list.size();
+            preferencesEditor.putInt(ALARMS, times);
+            preferencesEditor.commit();
+        }
+    }
+
+    public void deleteSchedule() {
+        if (intentArray.size() > 0) {
+            for (int i = 0; i < intentArray.size(); i++) {
+                alarmManager[i].cancel(intentArray.get(i));
+            }
+        }
+        intentArray.clear();
+        preferencesEditor.putInt(ALARMS, 0);
+        preferencesEditor.commit();
     }
 }
